ec12e648a483b41497cfee14dd6537ca2e3e59e3
Ing. Jan Kaláb <pitlicek@gmail.com>
2012-09-08 17:02:02 +0200

    Dary

---
+++ gifts.js
@@ -0,0 +1,18 @@
+var gifts = {
+//jméno: rezervováno
+"Osobní váha": true,
+"Konvice s filtrem vody": true,
+"Rychlovarná konvice": false,
+"Žehlička": false,
+"Žehlící prkno": false,
+"Vysavač": false,
+"Sklenice na víno": false,
+"Plech na pečení": false,
+"Pračka": false,
+"Myčka nádobí": false,
+"Lednice": false,
+"Televizor": false,
+"Talíře": false,
+"Příbory": false,
+"Mikrovlnka": false
+};
