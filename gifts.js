f7dbb1dfdc0a781d01ba8fb564105bd12a9a2fa5
Ing. Jan Kaláb <pitlicek@gmail.com>
2012-09-22 16:01:20 +0200

    Aktualizvany dary, vypnove texty, mse pro neverici, opravene styly.

--- gifts.js
+++ gifts.js
@@ -1,18 +1,25 @@
 var gifts = {
 //jméno: rezervováno
-"Osobní váha": true,
-"Konvice s filtrem vody": true,
+"Osobní váha": false,
+"Konvice s filtrem vody": false,
 "Rychlovarná konvice": false,
 "Žehlička": false,
 "Žehlící prkno": false,
+"Sušák na prádlo": false,
 "Vysavač": false,
 "Sklenice na víno": false,
 "Plech na pečení": false,
+"Nerezové hrnce": true,
 "Pračka": false,
 "Myčka nádobí": false,
 "Lednice": false,
-"Televizor": false,
-"Talíře": false,
-"Příbory": false,
-"Mikrovlnka": false
+//"Televizor (alespoň xy palců)": false,	//
+"Dózy (s víčkem) na mouku, cukr, atp.": false,
+"Plastové krabičky na potraviny (Tupperware)": true,
+"Váleček": false,
+"Mikrovlnka": false,	//Phillips, co ma mama
+"Akuvrtačka (NAREX ASV)": false,
+//Víc nářadí!
+"Matrace (délka alespoň 210 cm)": false,
+"Domácí pekárna": true
 };
