d18f3d0e52b2ed145d97677a2696657dc96b19ad
Ing. Jan Kaláb <pitlicek@gmail.com>
2012-09-29 10:58:19 +0200

    Dary na segry, oprava seznamu daru

--- gifts.js
+++ gifts.js
@@ -4,22 +4,32 @@ var gifts = {
 "Konvice s filtrem vody": false,
 "Rychlovarná konvice": false,
 "Žehlička": false,
-"Žehlící prkno": false,
+"Žehlící prkno (větší)": false,
 "Sušák na prádlo": false,
+"Zrcadlo": false,
 "Vysavač": false,
-"Sklenice na víno": false,
-"Plech na pečení": false,
-"Nerezové hrnce": true,
-"Pračka": false,
+"Ruční vysavač": true,
+"Smetáček + lopatka": false,
+"Smeták/koště": false,
+"Pračka": false,	//specifikovat rozměry!
 "Myčka nádobí": false,
-"Lednice": false,
-//"Televizor (alespoň xy palců)": false,	//
+//"Televizor (alespoň xy palců)": false,
 "Dózy (s víčkem) na mouku, cukr, atp.": false,
 "Plastové krabičky na potraviny (Tupperware)": true,
 "Váleček": false,
+"Sklenice všeho druhu": false,
+"Plech na pečení": false,
+"Dřevěné prkénko": false,
+"Nerezové hrnce": true,
 "Mikrovlnka": false,	//Phillips, co ma mama
 "Akuvrtačka (NAREX ASV)": false,
 //Víc nářadí!
-"Matrace (délka alespoň 210 cm)": false,
+"Jedno napínací prostěradlo 200×180 cm": false,
+"Druhé napínací prostěradlo 200×180 cm": false,
 "Domácí pekárna": true
+"Pumpička na gymbalón": false,
+"Doména": false,
+"Komoda MALM se 2 zásuvkami, bílá": false,	// http://www.ikea.com/cz/cs/catalog/products/90053918/ tohle v bílé?
+"Stolky do postele": true
+// ^^^ Posledni polozka nesmi mit carku! ^^^
 };
