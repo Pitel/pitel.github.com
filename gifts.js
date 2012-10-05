d04771835ec3b65c84d3d733518d9b2caeb60c94
Ing. Jan Kaláb <pitlicek@gmail.com>
2012-10-05 22:09:45 +0200

    Aktualizace daru

--- gifts.js
+++ gifts.js
@@ -3,7 +3,7 @@ var gifts = {
 "Osobní váha": false,
 "Konvice s filtrem vody": false,
 "Rychlovarná konvice": false,
-"Žehlička": false,
+"Žehlička": true,
 "Žehlící prkno (větší)": false,
 "Sušák na prádlo": false,
 "Zrcadlo": false,
@@ -11,13 +11,15 @@ var gifts = {
 "Ruční vysavač": true,
 "Smetáček + lopatka": false,
 "Smeták/koště": false,
-"Pračka": false,	//specifikovat rozměry!
+"Pračka (šíře 40 cm)": false,	//specifikovat rozměry!
 "Myčka nádobí": false,
 "Plochá televize s alespoň metrovou úhlopříčkou": false,
 "Dózy (s víčkem) na mouku, cukr, atp.": false,
-"Plastové krabičky na potraviny (Tupperware)": true,
+"Plastové krabičky na potraviny": true,
 "Váleček": false,
-"Sklenice všeho druhu": false,
+"Sada sklenic na pití": false,
+"Sada sklenic na bílé víno": false,
+"Sada sklenic na červené víno": false,
 "Plech na pečení": false,
 "Dřevěné prkénko": false,
 "Nerezové hrnce": true,
@@ -32,7 +34,6 @@ var gifts = {
 "Domácí pekárna": true,
 "Pumpička na gymbalón": false,
 "Doména": false,
-"Komoda MALM se 2 zásuvkami, bílá": false,	// http://www.ikea.com/cz/cs/catalog/products/90053918/ tohle v bílé?
 "Stolky do postele": true,
 "Skládací přepravky": false
 // ^^^ Posledni polozka nesmi mit carku! ^^^
